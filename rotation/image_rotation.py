c09933d28cf77921f4407fa3287717cbd44f73c5
rigi <lrpbaltazar@gmail.com>
2017-12-20 10:08:39 +0800

    add flipImage function

--- rotation/image_rotation.py
+++ rotation/image_rotation.py
@@ -10,4 +10,19 @@ def rotate_image(img_obj):
 	dst = cv2.warpAffine(img_obj, rot_matrix, (rows, cols))
 	return dst
 
+def flipImage(img):
+	flip_prob = np.random.uniform(0, 1)
+	if flip_prob < 0.25:
+		#does nothing to the image
+		flipped_img = img
+	elif flip_prob < 0.50:
+		#flips image horizontally
+		flipped_img = cv2.flip(img, 0)
+	elif flip_prob < 0.75:
+		#flips image vertically
+		flipped_img = cv2.flip(img, 1)
+	else:
+		#flips image both horizontally and vertically
+		flipped_img = cv2.flip(img, -1)
 
+	return flipped_img
