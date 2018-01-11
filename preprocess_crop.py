b43dd6afc64588426c74c435d09a924dda84060e
ririgi <lrpbaltazar@gmail.com>
2018-01-12 05:25:17 +0800

    add savetxt for no circles

--- preprocess_crop.py
+++ preprocess_crop.py
@@ -4,16 +4,21 @@ import glob
 import shutil
 import os
 import cv2
+import numpy as np
 
 location = '/Users/lpbaltazar/workspace/kaggle-data/train'
 file_type = '.jpeg'
 target_dir = '/Users/lpbaltazar/workspace/fundus-preprocessing/output_images/'
 i=0
+no_circles = []
 for n in glob.glob(location+'/*'+file_type):
 	img = adjustment.openImage(n)
 	crop, mes = croppingwithpad.cropImage(img, n)
-	#rot = rotate_image(crop, angle)
+	if mes == 1:
+		no_circles.append(n)
 	if mes == 0:
 		res = cv2.resize(crop, (512, 512))
 		cv2.imwrite(target_dir + 'image{:>05}.jpg'.format(i), res)
 		i = i+1
+
+np.savetxt('no_circles.txt', no_circles, delimeter = ",", fmt = "%s" )
