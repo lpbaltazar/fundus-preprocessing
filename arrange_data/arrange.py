3d08c8e8ab9c581b8a079f339afca524d41052d3
jdgaudillo <joverlyn.gaudillo@digenomix.com>
2018-01-12 16:57:16 +0800

    Adds comments to arrange.py and data.py

--- arrange_data/arrange.py
+++ arrange_data/arrange.py
@@ -4,27 +4,42 @@ import data
 
 filename = 'trainLabels.csv'
 
+# ROOT_PATH is the path of your python project.
 ROOT_PATH = "/Users/joverlyngaudillo/Desktop/diabetic-retinopathy"
+
+# data_directory is the full path of your dataset
 data_directory = os.path.join(ROOT_PATH, "INPUTDATA")
 
+# Fills the image_filenames and image_classes lists by 
+# calling the function load_labels() from data.py
 image_filenames, image_classes = data.load_labels(filename)
 
+# Iterates over the INPUTDATA directory
 for f in os.listdir(data_directory):
+	# Filters the content of the data_directory by choosing
+	# files that with .jpeg format
 	if f.endswith('.jpeg'):
+
+		# file_names is a list paths of classes directories
 		file_names = os.path.join(data_directory,f)
 
 		img = f.replace('.jpeg', '')
 
+		# Iterates over the image_filenames lists
 		for name in image_filenames:
 			if img == name:
 				break
 
+		# Gets the index of the image if matched
 		index = image_filenames.index(name)		
+		# Gets the class of the image
 		img_class = image_classes[index]
 
 		print('Filename:', name)
 		print('Image Class:', img_class)
 		
+		# Moves the images to a directory that is named after their class
+		# by calling the funtion moveImages() from data.py
 		data.moveImages(img_class, name + '.jpeg')
 
 		print('')
--- arrange_data/data.py
+++ arrange_data/data.py
@@ -1,11 +1,22 @@
+# Import needed modules
 import csv
 import shutil
 import os
 
+# Initiliaze variables to be filled later
 image_filename = []
 image_class = []
 
 def load_labels(filename):
+	""" This function loads the labels from the trainLabels.csv.
+
+	Arguments:
+			filename = name of the csv file (trainLabels.csv)
+
+	Returns: 
+			image_filename = list of the filenames of all the images in the dataset directory
+			labels = list of the labels of all the the images
+	"""
 	with open (filename, 'r') as csvfile:
 		file_reader = csv.reader(csvfile, delimiter = ',')
 		for row in file_reader:
@@ -17,6 +28,19 @@ def load_labels(filename):
 	return image_filename, image_class
 
 def moveImages(img_class, img_name):
+	""" This function moves images to a directory named after their corresponding class.
+		Calling this function will arrange your images according to their class.
+
+	Arguments:
+			img_class = class of the image
+			img_name = filename of the image
+
+	Requirements:
+			> Make a directory inside your project directory with the name INPUTDATA
+			> Inside this directory, make five directories with names 0, 1, 2, 3, 4, respectively
+
+	Returns: None
+	"""
 	source = '/Users/joverlyngaudillo/Desktop/diabetic-retinopathy/INPUTDATA'
 	img_path = os.path.join(source, img_name)
 
